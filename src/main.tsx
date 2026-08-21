6321488ba210f348f735f4f28f7633a84c8e5142
Tafi00 <tafizero2@gmail.com>
2026-08-21 13:27:43 +0700

    fix: bundle complete Montserrat latin glyphs

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import "@fontsource/montserrat/latin-400.css";
+import "@fontsource/montserrat/latin-500.css";
+import "@fontsource/montserrat/latin-600.css";
+import "@fontsource/montserrat/latin-700.css";
+import "@fontsource/montserrat/latin-800.css";
 import "@fontsource/montserrat/vietnamese-400.css";
 import "@fontsource/montserrat/vietnamese-500.css";
 import "@fontsource/montserrat/vietnamese-600.css";
